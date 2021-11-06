3ffabe84962562ca4c8eec32a88a16092a45761a
hasanslaravelproject <hasanslaravelproject@gmail.com>
2021-11-06 23:06:53 +0100

    first commit22

--- src/Api.js
+++ src/Api.js
@@ -2,12 +2,17 @@ import axios from "axios";
 const BaseUrl = 'http://127.0.0.1:8000/api/';
 const yourConfig = {
     headers: {
-       Authorization: "Bearer " + "1|gq3dTK1elWclrKJZhL64C9oktf4jmbxM49yqqaiU"
+       Authorization: "Bearer " + "1|TMpZviGCFIsXVIKXOX70e4yyRS6FQ05JJuCnIbFm"
     }
  };
 const API = {
-    makeGetRequest(url) {
-        return axios.get(BaseUrl+url, yourConfig);
+    makeGetRequest(url1, url2,url3) {
+        const clientApi = axios.get(BaseUrl+url1, yourConfig);
+        const allFoodsApi = axios.get(BaseUrl+url2, yourConfig);
+        const usersApi = axios.get(BaseUrl+url3, yourConfig);
+
+        return axios.all([clientApi, allFoodsApi,usersApi ])
+    
     } 
     } 
   export default API;
--- src/App.js
+++ src/App.js
@@ -1,39 +1,66 @@
+import React, { useState, useEffect } from "react";
+import Api from "./Api.js";
+import axios from "axios";
 
-import React, { useState, useEffect } from 'react'
-import Api from './Api.js';
 function App() {
-	const [loading, setLoading] = useState(false);
-	const [posts, setPosts] = useState([]);
-	useEffect(() => {
-		const loadPost = async () => {
-			setLoading(true);
-			 Api.makeGetRequest("users").then(
-				(res) => { 
-				 console.log("ASD",res.data.data);
-				   setPosts(res.data.data);
-				 } 
-			 );
-			 setLoading(false);
-		}
-		loadPost();
-	}, []);
-	return (
-		<>
-			<div className="App">
-			{posts.map((item,i) => {
+  const [loading, setLoading] = useState(false);
+  const [client, setClientInfo] = useState([]);
+  const [food, setFoodInfo] = useState([]);
+  const loadPost = async () => {
+    setLoading(true);
+    Api.makeGetRequest("clients", "all-food", "users").then(
+      axios.spread((...allData) => {
+        const clientData = allData[0].data.data;
+        const foodData = allData[1].data.data;
+
+        setClientInfo(clientData);
+        setFoodInfo(foodData);
+      })
+    );
+    setLoading(false);
+  };
+  useEffect(() => {
+    loadPost();
+  }, []);
+  return (
+    <>
+      <div className="App">
+        <table className="">
+          <thead className="">
+            <tr>
+              <th className="">Name</th>
+              {food.map((item) => {
                 return (
-                  <tr>
-                   <td>
-                      <strong>
-                        {item.name} 
-                      </strong>
-                    </td>
-                   </tr>
+                  <th className="" style={{ width: "200px" }}>
+                    <ul style={{ display: "inline", listStyleType: "none" }}>
+                      <li style={{}}>{item.name}</li>
+                    </ul>
+                  </th>
                 );
               })}
-			</div>
-		</>
-	);
+            </tr>
+          </thead>
+          <tbody>
+            {client.map((details) => {
+              return (
+                <tr>
+                  <td>
+                    <ul style={{ display: "block", listStyleType: "none" }}>
+                      <li>{details.name}</li>
+                    </ul>
+                  </td>
+                </tr>
+              );
+            })}
+
+            <td>0.3%</td>
+            <td>1.3%</td>
+            <td>5.3%</td>
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
 
 export default App;
